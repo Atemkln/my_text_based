ba599527d61a1fedefe7f3187b0a7fab327b71e1
Atemkln <adambeach16@gmail.com>
2022-01-31 18:25:47 -0500

    Story now in one file!

---
+++ story_data.py
@@ -0,0 +1,317 @@
+part1 = {
+    1: {
+      'Text': [
+            "Oh, uh hey.",
+            "I just wanted to say sorry about last night...",
+            "I usually don't get that drunk and sad, but its been a rough week.",
+            "...",
+            "Could you ever find it in your heart to forgive me?"
+        ],
+      'Options': [
+          ("Yes of course.", 2),
+          ("I'm sorry I dont remember anything. Who are you?", 3)
+        ]
+    },
+    2: {    
+        'Text': [
+            "Ah, that's good. That's...uh...",
+            "...",
+            "Anyways...",
+            "What should we do about it?",
+            "About, y'know, the thing."
+        ],
+        'Options': [
+          ("What thing? What are you talking about?", 4),
+          ("We should destroy it.", 5),
+          ("We should go get it.", 6)
+        ]
+    },
+    3: {
+        'Text': [
+            "Ah...darn",
+            "Yo!!",
+            "The PERSON is awake but they're acting all dopey and stuff!",
+            "...",
+            "Hey PERSON, any idea how to cure a hangover?"
+        ],
+        'Options': [
+          ("How did you get it?", 6),
+          ("Nah, no clue.", 5)
+        ]
+    },
+    4: {
+      'Text': [
+            "The what? The thing? What did I say-",
+            "Ohhh...",
+            "I meant the @#$%&! thing.",
+            "...You know what that is right?"
+        ],
+      'Options': [
+        ("Nah, no clue.", 5),
+        ("What do you mean?! All you showed me was a bunch of random characters!", 5)
+      ]
+    },
+    5: {
+      'Text': [
+            "Yeah, I figured you wouldn't know haha.",
+            "But whatever, I'm just messing with you.",
+            "In any case, answer me this:",
+            "You're in a burning building, and there's an orphanage on the 3rd floor...",
+            "And a retirement home on the 1st.",
+            "You can only save one person from one of the floors.", 
+            "Who are you picking?"
+        ],
+      'Options': [
+        ("I don't get it.", 6),
+        ("What does this have to do with where we are now?", 7)
+      ]
+    },
+    6: {
+      'Text': [
+            "Get it?",
+            "What the hell are you talking about?",
+            "Friendo...",
+            "Do you even know where you are right now?",
+        ],
+      'Options': [
+        ("...", 8),
+        ("...",8)
+        ]
+    },
+    7: {
+      'Text': [
+            "Hm...",
+            "Well friendo,",
+            "I guess you'll find out as soon as the flames find you.",
+        ],
+      'Options': [
+        ("...", 8),
+        ("...",8)
+      ]
+    },
+    8: {
+      'Text': [
+            "PART 1",
+            "BLAZE OF GLORY",
+            "I guess you'll find out as soon as the flames find you...",
+        ],
+      'Options': []
+    },
+}
+
+part2 = {
+  1: {
+      'Text': [
+            "The leaves blew hurridly across the concrete sidewalks.",
+            "Circling overhead was a dark and gloomy sky.",
+            "'These are mean streets' is how my partner would have put it.",
+            "Mean streets, indeed.",
+            "...",
+            "I need a gatorade.", 
+            "And a pepperette.",
+            "And a chocolate milk.",
+            "...",
+            "Haaah.",
+        ],
+      'Options': [
+          ("Go to the diner", 2),
+          ("Go to the corner store", 7),
+          ("Go to the convenience store", 8)
+        ]
+    },
+  2: {
+      'Text': [
+            "Why in the hell would I go to a diner for any of those things?",
+            "Other than the chocolate milk.",
+            "Which they probably have.",
+            "In any case, get your head on straight, muchacho."
+        ],
+      'Options': [
+          ("Go to the diner", 3),
+          ("Go to the corner store", 7),
+          ("Go to the convenience store", 8)
+        ]
+    },
+  3: {
+      'Text': [
+            "Guess chocolate milk is the only thing we're after then?",
+            "A little strange, but I guess whatever.",
+            "",
+            "",
+            "",
+            "Jessie's was bustling with activity.",
+            "It was the quintessential Greasy Spoon type deal.",
+            "One famous menu item, portions out the wazoo,",
+            "The staple of any small town.",
+            "Personally, I couldn't stand the place.",
+            "Too much sugar and milk,",
+            "Too little chocolate and accountability.",
+            "'One large chocolate milk,'",
+            "'Hold the chocolate, hold the milk.'", 
+            "'I need some information.'",
+            "Jessie nodded.",
+            "'Comin' right up.'",
+            "Jessie and I had an understanding."
+        ],
+      'Options': [
+          ("Flirt", 4),
+          ("Actually order a chocolate milk", 5),
+          ("Ask for information", 6)
+        ]
+    },
+  4: {
+      'Text': [
+            "???",
+            "You must be insane haha!",
+            "No wayy! We're just friends.",
+            "I would never do anything that would jeopardize this wonderful friendship.",
+            "...",
+            "That I think is great.",
+            "And I certainly wouldn't want to jeopardize.",
+            "..."
+        ],
+      'Options': [
+          ("Actually order a chocolate milk", 5),
+          ("Ask for information", 6),
+        ]
+    },
+  5: {
+      'Text': [
+            "'Actually, Jessie, I'll have the milk afterall,'",
+            "'They, they say ignorance is bliss,'",
+            "'And today's a day I'm feeling particularly blissful.'",
+            "*+10 to AWESOMENESS factor!*",
+            "",
+            "Jessie's door clooses behind me, and I am once again faced with a difficult choice."
+        ],
+      'Options': [
+          ("Go to the corner store", 3),
+          ("Go to the convenience store", 4)
+        ]
+    },
+  6: {
+      'Text': [
+            "'So what do you got for me?.'",
+            "Jessie's mouth folded into a grim half-smile.",
+            "'Take a look at this, @#$%&,",
+            "Jessie slid a note face-down across the counter,",
+            "'Recognize it?'"
+            "I flipped it over.",
+            "...",
+            "437 dollars...?"
+            "'And 59 cents.",
+            "???",
+            "",
+            "With my tab largely unpaid, I leave empty-handed."
+            "",
+            "*-1000 to AWESOMENESS factor!*",
+            "",
+            "...whatever."
+        ],
+      'Options': [
+          ("Go to the corner store", 7),
+          ("Go to the convenience store", 9)
+        ]
+    },
+    7: {
+      'Text': [
+            "It's been closed for years, try again.",
+            "Maybe in another lifetime you'll be able to relive the glory days of the good ol' corner store.",
+            "Its drinks were cold,",
+            "Its beef patties were fresh,",
+            "And it even had a gambling minigame!",
+            "Ah, crazy times..."
+        ],
+      'Options': [
+          ("Go to the convenience store", 9)
+        ]
+    },
+    9: {
+      'Text': [
+            "Ah yeah!",
+            "If there's any place in town that has all that stuff (except for the chocolate milk) it'll be there for sure.",
+            "",
+            "",
+            "",
+            "Circle Q was a contradiction in many ways.",
+            "For one thing, its logo was really more of an oval than a perfect circle.",
+            "The thing to know is that its called a 'convenience' store mostly out of irony.",
+            "The owner, Phineas, was hardly ever around,",
+            "So the place mostly operated on an 'honour system'.",
+            "Now you'd think that this would make it more convenient, right?",
+            "Wrong!!",
+            "You see, Phineas didn't bother with pricing anything,",
+            "So the amount you left in the cash register was the amount Phineas was going to get.",
+            "Problem is, in our town and our town alone, morality isn't an abstract concept;",
+            "Its a tangible thing that has serious consequences.",
+            "No I'm serious,",
+            "Like, watch this:",
+            "...",
+            "*-30 to MORALITY factor! >:(*",
+            "*You will be facing a .3 percent increase to your annual income taxes as per CODE 13 SECTION 6 of the...*",
+            "Anyways,",
+            "You get the idea.",
+            "What it means is that in the only place in town where you can get ANYTHING,",
+            "You either end up overpaying by wayyy too much in order to avoid MORALITY penalties,",
+            "Or you underpay, and end up putting your MORALITY score in the negatives.",
+            "And the worst part is,",
+            "That message, like the one that tells you when you've messed up,",
+            "It only appears, like, 30 percent of the time?",
+            "Beats me why they designed it that way...",
+            "",
+            "",
+            "The bell chimed as I walked in.",
+            "Its faded shelves never changed much,",
+            "And even when they did, nothing interesting was ever really on them.",
+            "Except for today, of course.",
+            "",
+            "PART 2",
+            "DOWNTRODDEN"
+        ],
+      'Options': []
+    }, 
+    9: {
+      'Text': [
+            "Ah yeah!",
+            "If there's any place in town that has all that stuff (except for the chocolate milk) it'll be there for sure.",
+            "",
+            "",
+            "",
+            "Circle Q was a contradiction in many ways.",
+            "For one thing, its logo was really more of an oval than a perfect circle.",
+            "The thing to know is that its called a 'convenience' store mostly out of irony.",
+            "The owner, Phineas, was hardly ever around,",
+            "So the place mostly operated on an 'honour system'.",
+            "Now you'd think that this would make it more convenient, right?",
+            "Wrong!!",
+            "You see, Phineas didn't bother with pricing anything,",
+            "So the amount you left in the cash register was the amount Phineas was going to get.",
+            "Problem is, in our town and our town alone, morality isn't an abstract concept;",
+            "Its a tangible thing that has serious consequences.",
+            "No I'm serious,",
+            "Like, watch this:",
+            "...",
+            "*-30 to MORALITY factor! >:(*",
+            "*You will be facing a .3 percent increase to your annual income taxes as per CODE 13 SECTION 6 of the...*",
+            "Anyways,",
+            "You get the idea.",
+            "What it means is that in the only place in town where you can get ANYTHING,",
+            "You either end up overpaying by wayyy too much in order to avoid MORALITY penalties,",
+            "Or you underpay, and end up putting your MORALITY score in the negatives.",
+            "And the worst part is,",
+            "That message, like the one that tells you when you've messed up,",
+            "It only appears, like, 30 percent of the time?",
+            "Beats me why they designed it that way...",
+            "",
+            "",
+            "The bell chimed as I walked in.",
+            "Its faded shelves never changed much,",
+            "And even when they did, nothing interesting was ever really on them.",
+            "Except for today, of course.",
+            "",
+            "PART 2",
+            "DOWNTRODDEN"
+        ],
+      'Options': []
+    }, 
+}
